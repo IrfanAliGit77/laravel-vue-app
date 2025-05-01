4dc122f6418b4ea97e31ca53a46abbec14f0ff27
IrfanAliGit77 <raygensh77@gmail.com>
2025-05-01 19:04:18 +0700

    make migration

---
+++ database/migrations/2025_05_01_120004_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->text('description');
+            $table->boolean('status')->default(true);
+            $table->dateTime('due_date')->nullable();
+            $table->json('metadata')->nullable();
+            $table->string('document')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
---
+++ database/migrations/2025_05_01_120055_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('task_id');
+            $table->text('comment');
+            $table->json('extra')->nullable();
+            $table->boolean('is_approved')->default(false);
+            $table->dateTime('commented_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
